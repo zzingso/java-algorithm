7fc4ec385993c1e36dfcf952422a737dbf96725a
zzingso <ravie90@gmail.com>
2021-03-08 08:47:09 +0000

    get .java file list WIP

--- Main.java
+++ Main.java
@@ -1,9 +1,39 @@
-import java.util.List;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 class Main {
+
+  public static final String JAVA = "java";
+  public static final String MAIN = "Main.java";
+
   public static void main(String[] args) {
-    FindPrimeNumber f = new FindPrimeNumber();
+
+    Scanner sc = new Scanner(System.in);
+    File file = new File("/home/runner/java-algorithm");
+    List<File> fileList = new ArrayList<File>();
+    int count = 1;
+
+    System.out.println("\n# Choose a Program ::\n");
+
+    for(File f: file.listFiles()){
+      String fileName = f.getName();
+      String ext = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+      if(JAVA.equals(ext) && !MAIN.equals(fileName)) {
+        fileList.add(f);
+        System.out.println(count + " : " + f.getName());
+        count++;
+      }
+    }
+
+    int selectedFile = sc.nextInt();
+    System.out.println(fileList.get(selectedFile - 1).getName());
+
+
+    //FindPrimeNumber f = new FindPrimeNumber();
     //System.out.println(f.solution("17"));
     //System.out.println(f.getResult("011"));
 
@@ -15,9 +45,9 @@ class Main {
     // int[] reserve = {2, 6};
     // System.out.println(s.solution(7, lost, reserve));
 
-    NewIdRecommend id = new NewIdRecommend();
+    //NewIdRecommend id = new NewIdRecommend();
     //bat.y.abcdefghi
-    System.out.println(id.solution("1...!@BaT#*..y.abcdefghijklm"));
+    //System.out.println(id.solution("1...!@BaT#*..y.abcdefghijklm"));
     
   }
 }
